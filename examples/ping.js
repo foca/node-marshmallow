5a92d35e29d51efada9daab14dd83d875d3a964d
Patrick Tulskie <patricktulskie@gmail.com>
2011-04-07 15:49:05 -0400

    Added a basic ping sample.

---
+++ examples/ping.js
@@ -0,0 +1,17 @@
+var marshmallow = require("../lib/marshmallow").marshmallow;
+
+var config = {
+  token:   process.env["CAMPFIRE_TOKEN"],
+  account: process.env["CAMPFIRE_ACCOUNT"],
+  ssl:     process.env["CAMPFIRE_SSL"] == "true",
+  room_id: process.env["CAMPFIRE_ROOM"]
+};
+
+marshmallow(config, function(bot) {
+  // Basic ping function
+  bot.on("^!ping", function(ping) {
+    this.speak("pong!");
+  });
+});
+
+
